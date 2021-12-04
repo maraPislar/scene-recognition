c7b4c7a40dcd721c72d53ff046b65b469f763197
Pranay Kotecha <pranay@pranaykotecha.com>
2021-12-04 14:27:50 +0000

    Tiny image classifier

--- First Classifier/src/main/java/uk/ac/soton/ecs/comp3204/App.java
+++ First Classifier/src/main/java/uk/ac/soton/ecs/comp3204/App.java
@@ -1,31 +1,60 @@
 package uk.ac.soton.ecs.comp3204;
 
+import org.apache.commons.vfs2.FileSystemException;
+import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.feature.DoubleFV;
 import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.FImage;
+import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
 import org.openimaj.image.colour.ColourSpace;
 import org.openimaj.image.colour.RGBColour;
 import org.openimaj.image.processing.convolution.FGaussianConvolve;
+import org.openimaj.image.processing.resize.ResizeProcessor;
 import org.openimaj.image.typography.hershey.HersheyFont;
+import org.openimaj.knn.DoubleNearestNeighboursExact;
+import org.openimaj.util.array.ArrayUtils;
+
+import java.util.*;
 
 /**
  * OpenIMAJ Hello world!
  *
  */
 public class App {
-    public static void main( String[] args ) {
-    	//Create an image
-        MBFImage image = new MBFImage(320,70, ColourSpace.RGB);
+    public static void main( String[] args ) throws FileSystemException {
+        VFSGroupDataset<FImage> trainingImages = new VFSGroupDataset<FImage>(
+                "C:/Users/prana/University/Computer Vision/Group Project/training/training",
+                ImageUtilities.FIMAGE_READER);
+        System.out.println(trainingImages.size());
 
-        //Fill the image with white
-        image.fill(RGBColour.WHITE);
-        		        
-        //Render some test into the image
-        image.drawText("Hello World", 10, 60, HersheyFont.CURSIVE, 50, RGBColour.BLACK);
+        TinyImageKNNClassifier classifier = new TinyImageKNNClassifier(trainingImages);
 
-        //Apply a Gaussian blur
-        image.processInplace(new FGaussianConvolve(2f));
-        
-        //Display the image
-        DisplayUtilities.display(image);
+        VFSListDataset<FImage> testingImages = new VFSListDataset<FImage>(
+                "C:/Users/prana/University/Computer Vision/Group Project/testing/testing",
+                ImageUtilities.FIMAGE_READER);
+
+        Map<String, String> resultMap = new HashMap<>();
+        for(int i = 0; i < testingImages.size(); i++) {
+            resultMap.put(testingImages.getID(i), classifier.classify(testingImages.get(i), 5));
+        }
+
+        String[] sortedResults = new String[resultMap.size()];
+        int j = 0;
+        for(Map.Entry<String, String> result : resultMap.entrySet()) {
+            sortedResults[j] = result.toString();
+            j++;
+        }
+
+        Arrays.sort(sortedResults);
+        Arrays.stream(sortedResults).forEach(System.out::println);
+
+
+        System.out.println(resultMap.size());
+
     }
+
+
+
 }
---
+++ First Classifier/src/main/java/uk/ac/soton/ecs/comp3204/TinyImageKNNClassifier.java
@@ -0,0 +1,80 @@
+package uk.ac.soton.ecs.comp3204;
+
+import org.apache.lucene.analysis.util.CharArrayMap;
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.feature.DoubleFV;
+import org.openimaj.image.FImage;
+import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.knn.DoubleNearestNeighboursExact;
+import org.openimaj.util.array.ArrayUtils;
+import org.openimaj.util.pair.IntDoublePair;
+
+import java.util.*;
+
+public class TinyImageKNNClassifier {
+    private final VFSGroupDataset<FImage> trainingImages;
+    private DoubleNearestNeighboursExact kNearestNeighbours;
+    private List<String> featureVectorGroups = new ArrayList<>();
+
+    public TinyImageKNNClassifier(VFSGroupDataset<FImage> trainingImages) {
+        this.trainingImages = trainingImages;
+        this.train();
+    }
+
+    private void train() {
+        List<double[]> featureVectors = new ArrayList<>();
+        for(String group : this.trainingImages.getGroups()) {
+            for (FImage trainingImage : this.trainingImages.get(group)) {
+                double[] featureVector = getFeatureVector(trainingImage);
+                featureVectors.add(featureVector);
+                featureVectorGroups.add(group);
+            } 
+        }
+        this.trainingImages.getGroups().forEach(group ->
+            this.trainingImages.get(group).forEach(trainingImage -> {
+                    featureVectors.add(getFeatureVector(trainingImage));
+                    featureVectorGroups.add(group);
+                }
+            ));
+
+        double[][] featureVectorsArray = new double[featureVectors.size()][featureVectors.get(0).length];
+        featureVectors.toArray(featureVectorsArray);
+
+        kNearestNeighbours = new DoubleNearestNeighboursExact(featureVectorsArray);
+    }
+
+    private static double[] getFeatureVector(FImage image) {
+        int size = Math.min(image.width, image.height);
+        FImage squareImage = image.extractCenter(size, size);
+        FImage tinyImage = squareImage.process(new ResizeProcessor(16, 16));
+
+        DoubleFV featureVector = new DoubleFV(ArrayUtils.reshape(ArrayUtils.convertToDouble(tinyImage.pixels)));
+        featureVector.normaliseFV();
+
+        return featureVector.values;
+    }
+
+    public String classify(FImage image, int k) {
+        double[] imageFeatureVector = getFeatureVector(image);
+        List<IntDoublePair> neighbours = kNearestNeighbours.searchKNN(imageFeatureVector, k);
+
+        Map<String, Integer> neighboursGroupCount = new HashMap<>();
+
+        for(IntDoublePair neighbour : neighbours) {
+            String result = this.featureVectorGroups.get(neighbour.getFirst());
+            int newGroupCount = neighboursGroupCount.getOrDefault(result, 0) + 1;
+            neighboursGroupCount.put(result, newGroupCount);
+        }
+
+        Map.Entry<String, Integer> highestEntry = null;
+        for(Map.Entry<String, Integer> groupCount : neighboursGroupCount.entrySet()) {
+            if(highestEntry == null || groupCount.getValue() > highestEntry.getValue()) {
+                highestEntry = groupCount;
+            }
+        }
+
+        assert highestEntry != null;
+        return highestEntry.getKey();
+    }
+}
