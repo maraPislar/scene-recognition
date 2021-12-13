a15edd965dfc9c4a913e6930a014ef9bd0b7f3f8
NikolStoeva <69462607+NikolStoeva@users.noreply.github.com>
2021-12-13 16:13:54 +0000

    Almost done. Still needs feature extraction.

--- Second Classifier/src/main/java/uk/ac/soton/ac/uk/3204/App.java
+++ Second Classifier/src/main/java/uk/ac/soton/ac/uk/3204/App.java
@@ -1,37 +1,26 @@
 package uk.ac.soton.ac.uk.3204;
 
 import org.apache.commons.vfs2.FileSystemException;
-import org.openimaj.data.DataSource;
-import org.openimaj.data.dataset.*;
-import org.openimaj.feature.DoubleFV;
-import org.openimaj.feature.FeatureExtractor;
+import org.openimaj.data.dataset.Dataset;
+import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.feature.FloatFV;
-import org.openimaj.feature.SparseIntFV;
 import org.openimaj.feature.local.LocalFeature;
 import org.openimaj.feature.local.LocalFeatureImpl;
 import org.openimaj.feature.local.SpatialLocation;
-import org.openimaj.feature.local.data.LocalFeatureListDataSource;
-import org.openimaj.feature.local.list.LocalFeatureList;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
-import org.openimaj.image.feature.dense.gradient.dsift.ByteDSIFTKeypoint;
-import org.openimaj.image.feature.dense.gradient.dsift.PyramidDenseSIFT;
 import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
-import org.openimaj.image.feature.local.aggregate.BlockSpatialAggregator;
-import org.openimaj.image.feature.local.keypoints.Keypoint;
 import org.openimaj.image.pixel.sampling.RectangleSampler;
-import org.openimaj.ml.clustering.ByteCentroidsResult;
+import org.openimaj.math.geometry.shape.Rectangle;
+import org.openimaj.ml.clustering.FloatCentroidsResult;
 import org.openimaj.ml.clustering.assignment.HardAssigner;
-import org.openimaj.ml.clustering.kmeans.ByteKMeans;
+import org.openimaj.ml.clustering.kmeans.FloatKMeans;
 import org.openimaj.util.array.ArrayUtils;
 import org.openimaj.util.pair.IntFloatPair;
-import org.openimaj.math.geometry.shape.Rectangle;
 
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Random;
 
 
 public class App {
@@ -40,85 +29,82 @@ public class App {
         VFSGroupDataset<FImage> images = new VFSGroupDataset<>("D:\\Uni\\Year 3\\Vision\\coursework3\\training", ImageUtilities.FIMAGE_READER);
         VFSGroupDataset<FImage> testing = new VFSGroupDataset<>("D:\\Uni\\Year 3\\Vision\\coursework3\\testing", ImageUtilities.FIMAGE_READER);
 
-        List<Rectangle> rectangleList = new ArrayList<>();
-        Map<FImage, List<Rectangle>> imageRecMap = new HashMap<>();
+        // Building the vocabulary with the training data
+        HardAssigner<float[], float[], IntFloatPair> assigner = trainQuantiliser(images);
 
-        //Patches from the images
-        for (FImage im : images) {
-            //takes the image and the window information
-            RectangleSampler rec = new RectangleSampler(im.normalise(), 4, 4, 8, 8);
-            imageRecMap.put(im, rec.allRectangles());
-
-            final List<LocalFeature<SpatialLocation, FloatFV>> areaList = new ArrayList<>();
-
-            for(Rectangle rectangle : rec.allRectangles()){
-                FImage patch = im.extractROI(rectangle);
-
-                //2D array to 1D array
-                final float[] vector = ArrayUtils.reshape(patch.pixels);
-                final FloatFV featureV = new FloatFV(vector);
-
-                //Location of rectangle is location of feature
-                final SpatialLocation sl = new SpatialLocation(rectangle.x, rectangle.y);
-
-                //Generate as a local feature for compatibility with other modules
-                final LocalFeature<SpatialLocation, FloatFV> lf = new LocalFeatureImpl<>(sl, featureV);
-
-                areaList.add(lf);
-            }
+        BagOfVisualWords<float[]> bagOfVisualWords = new BagOfVisualWords<>(assigner);
+        
+    }
 
 
-//            float[] locfeat = areaList.get(0).getFeatureVector().values;
-//            for(float f : locfeat){
-//                System.out.print(f + " ");
-//            }
+    // Method for patch extraction
+    static List<LocalFeature<SpatialLocation, FloatFV>> patchExtraction(FImage image){
 
-            break;
+        final List<LocalFeature<SpatialLocation, FloatFV>> patchList = new ArrayList<>();
+        RectangleSampler rec = new RectangleSampler(image.normalise(), 4, 4, 8, 8);
+
+        for(Rectangle rectangle : rec.allRectangles()){
+            FImage patch = image.extractROI(rectangle);
+
+            // Reshape a 2D array into a 1D array
+            final float[] vector = ArrayUtils.reshape(patch.pixels);
+            final FloatFV featureV = new FloatFV(vector);
+
+            // Assigning same location for rectangle and feature
+            final SpatialLocation sl = new SpatialLocation(rectangle.x, rectangle.y);
+
+            // Make the newly found feature local
+            final LocalFeature<SpatialLocation, FloatFV> lf = new LocalFeatureImpl<>(sl, featureV);
+
+            // Add the feature to a list
+            patchList.add(lf);
         }
-
-        List<LocalFeatureList<Keypoint>> allKeys = new ArrayList<>();
-
-        /* TODO libliniar anotator
-           TODO k-means clustering of patches?
-           TODO mean-centre and normalise patches before clustering
-         */
-
+        return patchList;
     }
 
 
 
-    static HardAssigner<byte[], float[], IntFloatPair> trainQuantiliser(Dataset<FImage> sample, PyramidDenseSIFT<FImage> pdsift){
-        List<LocalFeatureList<ByteDSIFTKeypoint>> allKeys = new ArrayList<>();
-        for(FImage rec : sample){
-            FImage image = rec.getImage();
-            pdsift.analyseImage(image);
-            allKeys.add(pdsift.getByteKeypoints(0.005f));
+    // Hard assigner from Chapter 12
+    static HardAssigner<float[], float[], IntFloatPair> trainQuantiliser(Dataset<FImage> sample){
+        List<float[]> allKeys = new ArrayList<>();
+
+        for(FImage img : sample){
+            FImage image = img.getImage();
+            // Getting a sample of patches
+            List<LocalFeature<SpatialLocation, FloatFV>> samplePatches = getRandomElements(patchExtraction(image), 15);
+            // Putting the vector values of the feature vectors in a list
+            for(LocalFeature<SpatialLocation, FloatFV> localFeature : samplePatches){
+                allKeys.add(localFeature.getFeatureVector().values);
+            }
         }
         if(allKeys.size() > 10000) allKeys = allKeys.subList(0,10000);
 
-        ByteKMeans km = ByteKMeans.createKDTreeEnsemble(300);
-        DataSource<byte[]> dataSource = new LocalFeatureListDataSource<>(allKeys);
-        ByteCentroidsResult result = km.cluster(dataSource);
+        FloatKMeans km = FloatKMeans.createKDTreeEnsemble(500);
 
+        float[][] dataSource = new float[allKeys.size()][];
+
+        for(int i = 0; i < allKeys.size(); i++){
+            dataSource[i] = allKeys.get(i);
+        }
+
+        FloatCentroidsResult result = km.cluster(dataSource);
         return result.defaultHardAssigner();
     }
 
 
+    // Method to get random patches form the feature list
+    static List<LocalFeature<SpatialLocation, FloatFV>> getRandomElements(List<LocalFeature<SpatialLocation, FloatFV>> patches, int numElements){
+        List<LocalFeature<SpatialLocation, FloatFV>> randomList = new ArrayList<>();
 
-    static class PHOWExtractor implements FeatureExtractor<DoubleFV, FImage>{
+        Random rand = new Random();
 
-        HardAssigner<byte[], float[], IntFloatPair> assigner;
+        for(int i = 0; i < numElements; i++){
+            int index = rand.nextInt();
 
-        @Override
-        public DoubleFV extractFeature(FImage object) {
-            FImage image = object.getImage();
-
-
-            BagOfVisualWords<byte[]> bagOfVisualWords = new BagOfVisualWords<>(assigner);
-            BlockSpatialAggregator<byte[], SparseIntFV> spatial = new BlockSpatialAggregator<>(bagOfVisualWords, 2, 2);
-
-            return null;
+            randomList.add(patches.get(index));
         }
+
+        return randomList;
     }
 }
 
