e68b0e12cfd6dbfa0370d4101402b81ff3a14217
Pranay Kotecha <pranay@pranaykotecha.com>
2021-12-16 04:52:44 +0000

    Testing images results to txt file and import cleanups

--- src/main/java/uk/ac/soton/ecs/comp3204/App.java
+++ src/main/java/uk/ac/soton/ecs/comp3204/App.java
@@ -1,11 +1,13 @@
 package uk.ac.soton.ecs.comp3204;
 
-import org.apache.commons.vfs2.FileSystemException;
-import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.data.dataset.*;
 import org.openimaj.experiment.dataset.split.GroupedRandomSplitter;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -13,15 +15,23 @@ import java.util.concurrent.atomic.AtomicInteger;
  *
  */
 public class App {
-    public static void main( String[] args ) throws FileSystemException {
-        VFSGroupDataset<FImage> images = new VFSGroupDataset<FImage>(
+    public static void main( String[] args ) throws IOException {
+        VFSGroupDataset<FImage> trainingImages = new VFSGroupDataset<FImage>(
                 "C:/Users/prana/University/Computer Vision/Group Project/training/training",
                 ImageUtilities.FIMAGE_READER);
 
         Classifier knnClassifier = new TinyImageKNNClassifier(5);
         Classifier bagOfWordsClassifier = new BagOfWordsClassifier();
-        //System.out.println(testClassifier(knnClassifier, images));
-        System.out.println(testClassifier(bagOfWordsClassifier, images));
+
+        VFSListDataset<FImage> testingImages = new VFSListDataset<FImage>(
+                "C:/Users/prana/University/Computer Vision/Group Project/testing/testing",
+                ImageUtilities.FIMAGE_READER);
+
+        outputResults(knnClassifier, trainingImages, testingImages, "run1.txt");
+        outputResults(bagOfWordsClassifier, trainingImages, testingImages, "run2.txt");
+
+        //System.out.println(testClassifier(knnClassifier, trainingImages));
+        //System.out.println(testClassifier(bagOfWordsClassifier, trainingImages));
     }
 
     private static double testClassifier(Classifier classifier, VFSGroupDataset<FImage> images) {
@@ -31,17 +41,38 @@ public class App {
         AtomicInteger correct = new AtomicInteger();
         AtomicInteger total = new AtomicInteger();
         splits.getTestDataset().getGroups().forEach(group ->
-                splits.getTestDataset().get(group).forEach(testingImage -> {
-                            if(classifier.classify(testingImage).equals(group)) {
-                                correct.getAndIncrement();
-                            }
-                            total.getAndIncrement();
-                        }
-                ));
+            splits.getTestDataset().get(group).forEach(testingImage -> {
+                if(classifier.classify(testingImage).equals(group)) {
+                    correct.getAndIncrement();
+                }
+                total.getAndIncrement();
+            })
+        );
 
         return correct.doubleValue() / total.doubleValue();
     }
 
+    private static void outputResults(Classifier classifier, VFSGroupDataset<FImage> trainingImages, VFSListDataset<FImage> testingImages, String filename) throws IOException {
+        GroupedDataset<String, ListDataset<FImage>, FImage> convertedTrainingImages = new MapBackedDataset<>();
+        convertedTrainingImages.putAll(trainingImages);
+        classifier.train(convertedTrainingImages);
 
+        String[] results = new String[testingImages.size()];
+        for(int i = 0; i < testingImages.size(); i++) {
+            results[i] = testingImages.getID(i) + " " + classifier.classify(testingImages.get(i));
+        }
+
+        Arrays.sort(results, (a, b) ->
+            Integer.parseInt(a.split("\\.")[0]) > Integer.parseInt(b.split("\\.")[0]) ? 1 : -1
+        );
+
+        FileWriter writer = new FileWriter("C:/Users/prana/University/Computer Vision/Group Project/" + filename);
+        for (String result : results) {
+            writer.write(result + "\n");
+        }
+        writer.close();
+
+        //Arrays.stream(results).forEach(System.out::println);
+    }
 
 }
