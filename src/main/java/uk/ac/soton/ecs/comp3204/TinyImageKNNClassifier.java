1333571a63641ae8d8d9fbe62e571018e9f28766
Pranay Kotecha <pranay@pranaykotecha.com>
2021-12-16 13:43:10 +0000

    Added comments to tinyImageKNN

--- src/main/java/uk/ac/soton/ecs/comp3204/TinyImageKNNClassifier.java
+++ src/main/java/uk/ac/soton/ecs/comp3204/TinyImageKNNClassifier.java
@@ -23,24 +23,31 @@ public class TinyImageKNNClassifier implements Classifier {
 
     @Override
     public void train(GroupedDataset<String, ListDataset<FImage>, FImage> trainingImages) {
+        //Iterate through every image of every group calculating the feature vector
         trainingImages.getGroups().forEach(group ->
             trainingImages.get(group).forEach(trainingImage -> {
                 this.featureVectors.add(getFeatureVector(trainingImage));
+                //Store corresponding group for reference later
                 this.featureVectorGroups.add(group);
             })
         );
 
+        //Convert from list to 2-d array
         double[][] featureVectorsArray = new double[this.featureVectors.size()][this.featureVectors.get(0).length];
         this.featureVectors.toArray(featureVectorsArray);
 
+        //Initialise NearestNeigbours
         this.kNearestNeighbours = new DoubleNearestNeighboursExact(featureVectorsArray);
     }
 
     private static double[] getFeatureVector(FImage image) {
+        //Calculate the smallest dimension
         int size = Math.min(image.width, image.height);
+        //Extract square image of this size and resize
         FImage squareImage = image.extractCenter(size, size);
         FImage tinyImage = squareImage.process(new ResizeProcessor(16, 16));
 
+        //Normalise feature vector
         DoubleFV featureVector = new DoubleFV(ArrayUtils.reshape(ArrayUtils.convertToDouble(tinyImage.pixels)));
         featureVector.normaliseFV();
 
@@ -49,17 +56,21 @@ public class TinyImageKNNClassifier implements Classifier {
 
     @Override
     public String classify(FImage image) {
+        //Convert image to classify into feature vector
         double[] imageFeatureVector = getFeatureVector(image);
         List<IntDoublePair> neighbours = this.kNearestNeighbours.searchKNN(imageFeatureVector, k);
 
         Map<String, Integer> neighboursGroupCount = new HashMap<>();
 
         for(IntDoublePair neighbour : neighbours) {
+            //Get group corresponding to each resultant feature vector by index
             String result = this.featureVectorGroups.get(neighbour.getFirst());
+            //Increment count
             int newGroupCount = neighboursGroupCount.getOrDefault(result, 0) + 1;
             neighboursGroupCount.put(result, newGroupCount);
         }
 
+        //Calculate group with highest number of "votes"
         Map.Entry<String, Integer> highestEntry = null;
         for(Map.Entry<String, Integer> groupCount : neighboursGroupCount.entrySet()) {
             if(highestEntry == null || groupCount.getValue() > highestEntry.getValue()) {
